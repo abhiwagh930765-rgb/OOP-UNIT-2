ebd2a65dec5764ba8fdf2f8e02336ed1b7c36c8e
abhiwagh930765-rgb <abhiwagh930765@gmail.com>
2026-09-26 19:14:56 +0530

    Refactor Vehicle classes with proper syntax

--- Program10.cpp
+++ Program10.cpp
@@ -1,34 +1,50 @@
+// Program 10: Demonstration of Function Overriding
+
 #include <iostream>
+using namespace std;
 
-class Vehicle {
+// Base class
+class Vehicle
+{
 public:
-    virtual void move() const {  // Virtual function for runtime polymorphism
-        std::cout << "Vehicle is moving\n";
-    }
-
-    virtual ~Vehicle() = default;
-};
-
-class Car : public Vehicle {  // Car inherits Vehicle
-public:
-    void move() const override {  // Override base class function
-        std::cout << "Car moves on roads\n";
+    // Virtual function
+    virtual void move()
+    {
+        cout << "Vehicle is moving" << endl;
     }
 };
 
-class Boat : public Vehicle {  // Boat inherits Vehicle
+// Car class
+class Car : public Vehicle
+{
 public:
-    void move() const override {  // Override base class function
-        std::cout << "Boat moves on water\n";
+    // Override function
+    void move() override
+    {
+        cout << "Car moves on roads" << endl;
     }
 };
 
-int main() {
+// Boat class
+class Boat : public Vehicle
+{
+public:
+    // Override function
+    void move() override
+    {
+        cout << "Boat moves on water" << endl;
+    }
+};
+
+int main()
+{
+    // Create objects
     Car car;
     Boat boat;
 
-    car.move();   // Calls Car's move()
-    boat.move();  // Calls Boat's move()
+    // Call overridden functions
+    car.move();
+    boat.move();
 
     return 0;
 }
